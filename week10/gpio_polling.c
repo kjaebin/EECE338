ef4b97fb02b9ee2c32b9dd8da1b018109a579d06
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-04-25 18:30:24 +0900

    Update gpio_polling.c

--- week10/gpio_polling.c
+++ week10/gpio_polling.c
@@ -16,7 +16,25 @@
 
 #define LOOP_PERIOD_MS 1000
 
+
 /* [P1] Write your function FROM here, if needed */
+
+#define SERVO_ANGLE_STEP 450
+
+int ledStates[5][3] = {
+    {1, 0, 0},  // Red
+    {0, 1, 0},  // Green
+    {0, 0, 1},  // Blue
+    {1, 1, 0},  // Yellow
+    {1, 0, 1}   // Magenta
+};
+
+void updateLEDColor() {
+    gpioWrite(PIN_LEDR, ledStates[currentColorIndex][0]);
+    gpioWrite(PIN_LEDG, ledStates[currentColorIndex][1]);
+    gpioWrite(PIN_LEDB, ledStates[currentColorIndex][2]);
+    currentColorIndex = (currentColorIndex + 1) % 5;
+}
 
 /* [P1] Write your function UP TO here, if needed */
 
@@ -24,6 +42,12 @@
 int change_servo_angle(int servo_state){
     int servo_angle;
 	/* [P1] Write your function for servo*/
+
+    int servo_angle = SERVO_POS_MIN + *servoState * SERVO_ANGLE_STEP;
+    if (servo_angle > SERVO_POS_MAX || servo_angle < SERVO_POS_MIN) {
+        *servoState = 0;  // Reset state if out of bounds
+        servo_angle = SERVO_POS_MIN;
+    }
 
 	/* [P1] Write your function FROM here*/
     return servo_angle;
@@ -36,6 +60,9 @@ int main()
     unsigned long t_start_ms, t_elapsed_ms;
 
     /* [P1] Write your variables FROM here */
+
+    int currentColorIndex = 0;
+    int servoState = 0;
 
     /* [P1] Write your variables UP TO here */
 
@@ -68,6 +95,18 @@ int main()
 
         // If the button is pushed, change the color of the LED. Be
         // sure the LED color switches between five or more colors.
+
+        // Update servo angle
+        int servo_angle = change_servo_angle(servoState);
+        gpioServo(PIN_SERVO, servo_angle);
+        servoState = (servoState + 1) % 5;  // Update state for next iteration
+
+        // Read the button pin state
+        int buttonState = gpioRead(PIN_BTN);
+        printf("Button state: %d\n", buttonState);
+        if (buttonState == PI_LOW) {  // If the button is pushed
+            updateLEDColor();
+        }
 
         /* [P1] Write your codes UP TO here */
 

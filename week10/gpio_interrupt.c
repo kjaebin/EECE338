372b97b514c5180f078f0e9a8d0111a4a48206a7
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-04-25 19:25:12 +0900

    Update gpio_interrupt.c

--- week10/gpio_interrupt.c
+++ week10/gpio_interrupt.c
@@ -14,63 +14,67 @@
 #define SERVO_POS_MAX 2000
 #define LOOP_PERIOD_MS 1000
 
-
 /* [P2] Write your global variables FROM here*/
-
-volatile int ledColorIndex = 0;  // Used to track current LED color index
-int ledStates[5][3] = {  // RGB states for different colors
-    {1, 0, 0},  // Red
-    {0, 1, 0},  // Green
-    {0, 0, 1},  // Blue
-    {1, 1, 0},  // Yellow
-    {1, 0, 1}   // Magenta
-};
-volatile int servoState = 0;  // Used to track current servo position state
-
+volatile int btn_state = -1;
+volatile int led_state = 0;
 /* [P2] Write your global variables UP TO here*/
 
-
-
 /* [P2] Write your function FROM here, if needed */
-
-void updateLEDColor() {
-    gpioWrite(PIN_LEDR, ledStates[ledColorIndex][0]);
-    gpioWrite(PIN_LEDG, ledStates[ledColorIndex][1]);
-    gpioWrite(PIN_LEDB, ledStates[ledColorIndex][2]);
-    ledColorIndex = (ledColorIndex + 1) % 5;  // Cycle through colors
+void change_led_color() {
+    // Rotate through 7 different color combinations
+    led_state = (led_state + 1) % 7;
+    switch(led_state) {
+        case 0:
+            gpioWrite(PIN_LEDR, PI_LOW);
+            gpioWrite(PIN_LEDG, PI_LOW);
+            gpioWrite(PIN_LEDB, PI_LOW);
+            break;
+        case 1:
+            gpioWrite(PIN_LEDR, PI_HIGH);
+            gpioWrite(PIN_LEDG, PI_LOW);
+            gpioWrite(PIN_LEDB, PI_LOW);
+            break; 
+        case 2:
+            gpioWrite(PIN_LEDR, PI_LOW);
+            gpioWrite(PIN_LEDG, PI_HIGH);
+            gpioWrite(PIN_LEDB, PI_LOW);
+            break;   
+        case 3:
+            gpioWrite(PIN_LEDR, PI_LOW);
+            gpioWrite(PIN_LEDG, PI_LOW);
+            gpioWrite(PIN_LEDB, PI_HIGH);
+            break;
+        case 4:
+            gpioWrite(PIN_LEDR, PI_HIGH);
+            gpioWrite(PIN_LEDG, PI_HIGH);
+            gpioWrite(PIN_LEDB, PI_LOW);
+            break;
+        case 5:
+            gpioWrite(PIN_LEDR, PI_HIGH);
+            gpioWrite(PIN_LEDG, PI_LOW);
+            gpioWrite(PIN_LEDB, PI_HIGH);
+            break;
+        case 6:
+            gpioWrite(PIN_LEDR, PI_LOW);
+            gpioWrite(PIN_LEDG, PI_HIGH);
+            gpioWrite(PIN_LEDB, PI_HIGH);
+            break;
+    }
 }
-
 /* [P2] Write your function UP TO here, if needed */
 
-void myISR()
-{
-    btn_state = gpioReadDebounce(PIN_BTN);
-    if (btn_state < 0) // If the input is noise, do nothing
-        return;
-
-    printf("Interrupt! Button state: %d\r\n", btn_state);
-
-    /*** [P2] Write your code FROM here ***/
-    // If the button is pushed, change the color of the LED. Be
-    // sure the LED color switches between five or more colors.
-
-    if (btn_state == 1) {  // Check if the button is pressed
-        updateLEDColor();  // Change LED color
+void myISR() {
+    btn_state = gpioRead(PIN_BTN);
+    if(btn_state == PI_LOW) { // Assuming the button press pulls the pin low
+        printf("Interrupt! Button state: %d\r\n", btn_state);
+        change_led_color();
     }
-
-    /*** [P2] Write your code UP TO here ***/
 }
 
-int main()
-{
+int main() {
     unsigned long t_start_ms, t_elapsed_ms;
 
-    /* [P2] Write your variables FROM here, if needed */
-
-    /* [P2] Write your variables UP TO here, if needed */
-
-    // GPIO settings
-    if (gpioInitialise() < 0) {
+    if(gpioInitialise() < 0) {
         printf("Cannot initialize GPIOs\r\n");
         return 1;
     }
@@ -85,25 +89,11 @@ int main()
     gpioWrite(PIN_LEDG, PI_LOW);
     gpioWrite(PIN_LEDB, PI_LOW);
 
-    // Interrupt settings
     gpioSetISRFunc(PIN_BTN, EITHER_EDGE, 0, myISR);
 
-    // Infinite loop
-    while (1) {
+    while(1) {
         t_start_ms = millis();
-
-        /*** [P2] Delete all codes in between and write your code FROM here ***/
-
-        int servo_angle = SERVO_POS_MIN + servoState * SERVO_ANGLE_STEP;
-        if (servo_angle > SERVO_POS_MAX || servo_angle < SERVO_POS_MIN) {
-            servoState = 0; // Reset state if out of bounds
-            servo_angle = SERVO_POS_MIN;
-        }
-        gpioServo(PIN_SERVO, servo_angle);
-        servoState = (servoState + 1) % 5;  // Increment state for next iteration
-
-        /*** [P2] Write your code UP TO here ***/
-
+        // Main loop body remains empty as all control is interrupt driven
         t_elapsed_ms = millis() - t_start_ms;
         sleep_ms(LOOP_PERIOD_MS - t_elapsed_ms);
     }
